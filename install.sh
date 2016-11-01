c195b08230feea80d8901b28a595b123676cc0b3
Taylor Burgess <tayloraburgess@gmail.com>
2016-11-01 16:51:38 -0400

    Add install script

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#
+# Dotfiles Install Script
+#
+
+ln -s ~/dotfiles/.bash_profile ~/.bash_profile
+ln -s ~/dotfiles/.bashrc ~/.bashrc
+ln -s ~/dotfiles/.vimrc ~/.vimrc
+ln -s ~/dotfiles/.tern-config ~/.tern-config
