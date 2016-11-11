ff0b43a5c0ce1ab330161d5300a447f75c2b7993
Taylor Burgess <tayloraburgess@gmail.com>
2016-11-11 14:23:00 -0500

    Update vim_1.sh to include DIR
    Script lacked declaration of DIR variable, and wasn't properly
    linking monokai.vim as a result. Declare the DIR variable.

--- vim_1.sh
+++ vim_1.sh
@@ -4,7 +4,11 @@
 # Vim Setup 1
 #
 
+# Set DIR variable to this directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 # Create link to Vim theme
+mkdir ~/.vim/colors
 ln -s "${DIR}/monokai.vim" ~/.vim/colors/monokai.vim
 
 # Install Vim Plug
