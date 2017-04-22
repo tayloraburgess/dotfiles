01f43436f104ae9e778f75c68bdc680cfee13bbc
Taylor Burgess <tayloraburgess@gmail.com>
2017-04-22 19:09:04 -0400

    Add key repeat to `install.sh`

--- install.sh
+++ install.sh
@@ -14,3 +14,7 @@ ln -s "${DIR}/.vimrc" ~/.vimrc
 ln -s "${DIR}/.tern-config" ~/.tern-config
 mkdir ~/.hammerspoon
 ln -s "${DIR}/init.lua" ~/.hammerspoon/init.lua
+
+if [ "$(uname)" == "Darwin" ]; then
+    defaults write -g KeyRepeat -float 1.1
+fi
