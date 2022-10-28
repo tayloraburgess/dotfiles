b5aacd8eea047edb0ee67c2e9291f43d88a14c13
Taylor Burgess <tayloraburgess@gmail.com>
2022-10-28 18:32:59 -0400

    update stuff

--- install.sh
+++ install.sh
@@ -21,17 +21,11 @@ brew install --cask firefox
 brew install --cask signal
 brew install --cask arq
 brew install --cask rectangle
-brew install --cask rekordbox
 brew install --cask discord
-brew install --cask ilok-license-manager
 brew install --cask calibre
 brew install --cask nordvpn
-brew install --cask izotope-product-portal
-brew install --cask little-snitch
-brew install --cask micro-snitch
 brew install --cask logitech-options
 brew install --cask 1password
-brew install --cask loopback
 brew install --cask obsidian
 brew install --cask transmission
 brew install --cask zoom
